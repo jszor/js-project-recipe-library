040c8f838e4fac806b56cbd9eba2262488f11d28
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-03-05 23:07:27 +0100

    added logic for selecting filter buttons, and minor style changes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Filter button selection logic
 
-let buttons = Array.from(document.getElementsByClassName("filter-button"));
-let allFoodButton = document.getElementById("all-button");
+const filterButtons = Array.from(document.getElementsByClassName("filter-button"));
+const allFoodButton = document.getElementById("all-button");
 
-buttons.forEach(button => {
+filterButtons.forEach(button => {
   button.addEventListener("click", () => selectFilter(button));
 });
 
 const selectFilter = (button) => {
   if (button === allFoodButton) {
-    buttons.forEach(button => button.classList.remove("active-filter"));
+    filterButtons.forEach(button => button.classList.remove("active-filter"));
     allFoodButton.classList.add("active-filter");
   } else {
     allFoodButton.classList.remove("active-filter");
@@ -20,4 +20,41 @@ const selectFilter = (button) => {
   } else {
     button.classList.add("active-filter");
   }
+}
+
+// Sort button selection logic
+
+const sortButtons = Array.from(document.getElementsByClassName("sorting-button"));
+const sortStates = {};
+const sortingEmojis = [" ↕️", " ⬆️", " ⬇️"];
+
+sortButtons.forEach(button => {
+  sortStates[button.innerText] = 0;
+  button.innerText += sortingEmojis[0];
+  button.addEventListener("click", () => selectSorting(button));
+});
+
+const selectSorting = (button) => {
+
+  let buttonText = button.innerText.split(" ")[0];
+
+  sortButtons.forEach(button => {
+    button.classList.remove("active-sorting");
+  })
+
+  if (sortStates[buttonText] === 0) {
+    sortStates[buttonText] = 1;
+    button.innerText = buttonText + sortingEmojis[1];
+  } else if (sortStates[buttonText] === 1) {
+    sortStates[buttonText] = 2;
+    button.innerText = buttonText + sortingEmojis[2];
+  } else {
+    sortStates[buttonText] = 0;
+    button.innerText = buttonText + sortingEmojis[0];
+  }
+
+  if (sortStates[buttonText] !== 0) {
+    button.classList.add("active-sorting");
+  }
+
 }
